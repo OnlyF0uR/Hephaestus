c25592f32edc582c2b523da461b874aa2ad9c107
OnlyF0uR <f0uronvacation@protonmail.com>
2022-07-21 11:23:24 +0200

    Even more improvements to the setup file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 # /bin/bash
 
-mkdir -p build
+if [ -d "./build" ]
+then
+	echo "Build folder already exists."
+else
+	mkdir build
+	
+	echo "go build -o heph ../src/main.go" > build/build.sh
+	chmod +x build/build.sh
 
-echo "go build -o heph ../src/main.go" > build/build.sh
-chmod +x build/build.sh
+	echo "Browse to ./build and run the build.sh file to build the binary."
+fi

e7aa825c3b451a37e0d78eec456ad0ac3b922df9
OnlyF0uR <f0uronvacation@protonmail.com>
2022-07-22 15:17:10 +0200

    Setup script fix

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ then
 	echo "Build folder already exists."
 else
 	mkdir build	
-	echo "go build -o heph -ldflags="-s -w" ../src/main.go" > build/build.sh
+	echo 'go build -o heph -ldflags="-s -w" ../src/main.go' > build/build.sh
 	chmod +x build/build.sh
 
 	echo "Browse to ./build and run the build.sh file to build the binary."
